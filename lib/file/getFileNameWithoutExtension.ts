444730892cecaeeb803b76b060596a059d7b0b3e
AvatarN03 <jsprashanth003@gmail.com>
2026-07-25 14:45:37 +0530

    feat: remove deprecated file and authentication hooks

--- lib/file/getFileNameWithoutExtension.ts
+++
@@ -1,5 +0,0 @@
-// const getFileNameWithoutExtension = (file: File): string => {
-//   return file.name.replace(/\.[^/.]+$/, "");
-// };
-
-// export default getFileNameWithoutExtension;
--- lib/formatTime.ts
+++
@@ -1,8 +0,0 @@
-export const formatTime = (seconds: number) => {
-  const mins = Math.floor(seconds / 60);
-  const secs = seconds % 60;
-
-  if (mins === 0) return `${secs}s`;
-
-  return `${mins}:${secs.toString().padStart(2, "0")}`;
-};
